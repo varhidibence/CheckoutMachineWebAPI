616dab353ec806043a99da042aafa2dd4176518c
Várhidi Bence <47637990+varhidibence@users.noreply.github.com>
2022-10-15 11:47:24 +0200

    add CheckoutMachineService

---
+++ CheckoutMachineWebAPI/Services/CheckoutMachineService.cs
@@ -0,0 +1,17 @@
+﻿using CheckoutMachineWebAPI.Interfaces;
+
+namespace CheckoutMachineWebAPI.Services
+{
+  public class CheckoutMachineService : ICheckoutMachineService
+  {
+    public KeyValuePair<string, int> Checkout(KeyValuePair<string, int> inserted, int price)
+    {
+      throw new NotImplementedException();
+    }
+
+    public KeyValuePair<string, int> GetStoredItems()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
