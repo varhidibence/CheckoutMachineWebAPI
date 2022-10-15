099a1602a7c5e0e725c9ca7075e18f06c68a035c
Várhidi Bence <47637990+varhidibence@users.noreply.github.com>
2022-10-15 19:09:48 +0200

    added unit tests

---
+++ CheckoutMachineWebAPI.Tests/CheckoutMachineServiceTests.cs
@@ -0,0 +1,90 @@
+﻿using CheckoutMachineWebAPI.Interfaces;
+using CheckoutMachineWebAPI.Models;
+using CheckoutMachineWebAPI.Services;
+
+namespace CheckoutMachineWebAPI.Tests
+{
+  public class CheckoutMachineServiceTests
+  {
+    [Fact]
+    public void TestCheckout_AmountEqualPrice()
+    {
+      // Arrange
+      CheckoutMachineService service = new CheckoutMachineService();
+      service._currencies = new List<ICurrency>() { new Currency { Amount = 1, Denomination = "1000" } };
+
+      var insertedMoney = new Dictionary<string, int>
+      {
+        { "1000", 1 }
+      };
+
+      TransactionData transactionData = new TransactionData()
+      {
+        Inserted = insertedMoney,
+        Price = 1000
+      };
+
+
+      // Act
+      var cashBack = service.Checkout(transactionData);
+      var amount = service._currencies.FirstOrDefault(curr => curr.Denomination == "1000")?.Amount;
+
+      // Assert
+      Assert.Empty(cashBack);
+      Assert.Equal(2, amount);
+    }
+
+    [Fact]
+    public void TestCheckout_ProperCashback()
+    {
+      // Arrange
+      CheckoutMachineService service = new CheckoutMachineService();
+      service._currencies.Add( new Currency { Amount = 2, Denomination = "200" });
+      service._currencies.Add( new Currency { Amount = 2, Denomination = "100" });
+
+      var insertedMoney = new Dictionary<string, int>
+      {
+        { "1000", 1 }
+      };
+
+      TransactionData transactionData = new TransactionData()
+      {
+        Inserted = insertedMoney,
+        Price = 700
+      };
+
+
+      // Act
+      var cashBack = service.Checkout(transactionData);
+      var actual = CheckoutMachineService.DotProduct(cashBack);
+
+      // Assert
+      Assert.Equal(300, actual);
+    }
+
+    [Fact]
+    public void TestCheckout_CannotGiveCashback()
+    {
+      // Arrange
+      CheckoutMachineService service = new CheckoutMachineService();
+      service._currencies.Add(new Currency { Amount = 2, Denomination = "200" });
+
+      var insertedMoney = new Dictionary<string, int>
+      {
+        { "1000", 1 }
+      };
+
+      TransactionData transactionData = new TransactionData()
+      {
+        Inserted = insertedMoney,
+        Price = 700
+      };
+
+      // Act
+      var ex = Assert.Throws<Exception>(() => { service.Checkout(transactionData); });
+
+      // Assert
+      Assert.Equal(typeof(Exception), ex.GetType());
+    }
+  }
+}
