59914dae869251dc770078db63c0b4a7b9d8561e
Várhidi Bence <47637990+varhidibence@users.noreply.github.com>
2022-10-15 12:26:01 +0200

    Added currency model

---
+++ CheckoutMachineWebAPI/Models/Currency.cs
@@ -0,0 +1,11 @@
+﻿using CheckoutMachineWebAPI.Interfaces;
+
+namespace CheckoutMachineWebAPI.Models
+{
+  public class Currency : ICurrency
+  {
+    public string? Value {get; set;}
+
+    public int Amount {get; set;} 
+  }
+}
